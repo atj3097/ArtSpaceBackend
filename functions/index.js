6e51c05ab870bad2196333486dcf8089d3d22880
Adam <adamjackson@pursuit.org>
2020-04-10 23:36:36 -0400

    Capability to add debit cards and create payments

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,37 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin'); 
 admin.initializeApp(); 
 // const logging = require('@google-cloud/logging')();
-const stripe = require('stripe')('sk_test_03keKEvzEQkLMwegjjqG5i8n00MFIzSWzB') 
+const stripe = require('stripe')('sk_test_03keKEvzEQkLMwegjjqG5i8n00MFIzSWzB')   
+const currency = 'usd';
+
+// [START chargecustomer]
+// Charge the Stripe customer whenever an amount is written to the Realtime database
+exports.createStripeCharge = functions.firestore.document('stripe_customers/{userId}/charges/{id}').onCreate(async (snap, context) => {
+      const val = snap.data();
+      try {
+        // Look up the Stripe customer id written in createStripeCustomer
+        const snapshot = await admin.firestore().collection(`stripe_customers`).doc(context.params.userId).get()
+        const snapval = snapshot.data();
+        const customer = snapval.customer_id
+        // Create a charge using the pushId as the idempotency key
+        // protecting against double charges
+        const amount = val.amount;
+        const idempotencyKey = context.params.id;
+        const charge = {amount, currency, customer};
+        if (val.source !== null) {
+          charge.source = val.source;
+        }
+        const response = await stripe.charges.create(charge, {idempotency_key: idempotencyKey});
+        // If the result is successful, write it back to the database
+        return snap.ref.set(response, { merge: true });
+      } catch(error) {
+        // We want to capture errors and render them in a user-friendly way, while
+        // still logging an exception with StackDriver
+        console.log(error);
+        // await snap.ref.set({error: userFacingMessage(error)}, { merge: true });
+        // return reportError(error, {user: context.params.userId});
+      }
+    });
 
 exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => { 
   //await - stops functioning from continuing to run while waiting for data
@@ -10,64 +40,23 @@ exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
     return admin.firestore().collection('stripe_customers').doc(user.uid).set({customer_id: customer.id});
   });
   
-  exports.addPaymentSource = functions.firestore.document('/stripe_customers/{userId}/tokens/{pushId}').onCreate(async (snap, context) => {
-    const source = snap.data();
-    const token = source.token;
-    if (source === null){
-      return null;
-    }
-  
-    try {
-      const snapshot = await admin.firestore().collection('stripe_customers').doc(context.params.userId).get(); 
-      console.log('Getting documents');
-      const customer =  snapshot.data().customer_id; 
-      console.log(token.id)
-      const response = await stripe.customers.createSource(customer, {source: token}); 
-      //Setting the source  
-      console.log('Payment Set in Stripe');
-      return admin.firestore().collection('stripe_customers').doc(context.params.userId).collection("sources").doc(response.fingerprint).set(response, {merge: true});
-    } catch (error) {
-      // await snap.ref.set({'error':userFacingMessage(error)},{merge:true});
-      // return reportError(error, {user: context.params.userId}); 
-      return error;
-    }
-  }); 
-  // function reportError(err, context = {}) {
-  //   // This is the name of the StackDriver log stream that will receive the log
-  //   // entry. This name can be any valid log stream name, but must contain "err"
-  //   // in order for the error to be picked up by StackDriver Error Reporting.
-  //   const logName = 'errors';
-  //   const log = logging.log(logName);
-  
-  //   // https://cloud.google.com/logging/docs/api/ref_v2beta1/rest/v2beta1/MonitoredResource
-  //   const metadata = {
-  //     resource: {
-  //       type: 'cloud_function',
-  //       labels: {function_name: process.env.FUNCTION_NAME},
-  //     },
-  //   };
-  
-  //   // https://cloud.google.com/error-reporting/reference/rest/v1beta1/ErrorEvent
-  //   const errorEvent = {
-  //     message: err.stack,
-  //     serviceContext: {
-  //       service: process.env.FUNCTION_NAME,
-  //       resourceType: 'cloud_function',
-  //     },
-  //     context: context,
-  //   };
-  
-  //   // Write the error log entry
-  //   return new Promise((resolve, reject) => {
-  //     log.write(log.entry(metadata, errorEvent), (error) => {
-  //       if (error) {
-  //        return reject(error);
-  //       }
-  //       return resolve();
-  //     });
-  //   });
-  // } 
+// Add a payment source (card) for a user by writing a stripe payment source token to Realtime database
+exports.addPaymentSource = functions.firestore.document('/stripe_customers/{userId}/tokens/{pushId}').onCreate(async (snap, context) => {
+  const source = snap.data();  
+  console.log(context)
+  const token = context.params.pushId; 
+  console.log(token)
+  if (source === null){
+    return null;
+  }
 
-  // function userFacingMessage(error) {
-  //   return error.type ? error.message : 'An error occurred, developers have been alerted';
-  // }
+  try {
+    const snapshot = await admin.firestore().collection('stripe_customers').doc(context.params.userId).get(); 
+    const customer =  snapshot.data().customer_id; 
+    console.log(customer) 
+    const response = await stripe.customers.createSource(customer, {source: token}); 
+    return admin.firestore().collection('stripe_customers').doc(context.params.userId).collection("sources").doc(response.fingerprint).set(response, {merge: true});
+  } catch (error) {
+    console.log(error)
+  }
+});
